553b774d75c0495a3c1020527a4e8e5db22a5131
Baldur Bjarnason <bb@baldurbjarnason.com>
2026-01-09 21:40:03 +0000

    Update init-theme.js

--- javascripts/discourse/api-initializers/init-theme.js
+++ javascripts/discourse/api-initializers/init-theme.js
@@ -1,4 +1,5 @@
 import { apiInitializer } from "discourse/lib/api";
+import Topic from "discourse/models/topic";
 
 export default apiInitializer((api) => {
 	api.onPageChange((url, title) => {
@@ -8,6 +9,7 @@ export default apiInitializer((api) => {
 			// 	$elem.children(".discourse-root").addClass("rebus-front-page"),
 			// );
 			document.documentElement.classList.remove("rebus-sub-page");
+			setupTopics(api);
 		} else {
 			document.documentElement.classList.add("rebus-sub-page");
 		}
@@ -18,3 +20,13 @@ export default apiInitializer((api) => {
 	api.replaceIcon("notification.liked_2", "far-heart");
 	api.replaceIcon("notification.liked_many", "far-heart");
 });
+
+async function setupTopics(api) {
+	const result = await fetch("/latest.json");
+	const topics = await result.json();
+	const featuredTopics = topics.topic_list.topics.map((topic) => {
+		return Topic.create(topic);
+	});
+	console.log(featuredTopics);
+	return featuredTopics;
+}
